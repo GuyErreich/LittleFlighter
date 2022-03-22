025caf47c45406e76e7b80f9c4d594a041cad9fd
GuyErriech <funfunmo17@gmail.com>
2022-03-22 17:28:02 +0200

    Modified EnemyPointer script

--- Assets/LittleFlighter/Scripts/EnemyPointer.cs
+++ Assets/LittleFlighter/Scripts/EnemyPointer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyPointer : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    private Vector3 tragetPosition;
+    private Vector3 tragetPosition, centerOfScreen;
     private RectTransform pointerRectTransform;
     private bool isOffScreen;
 
@@ -13,14 +13,22 @@ public class EnemyPointer : MonoBehaviour
     private void Awake()
     {
         this.pointerRectTransform = this.transform.Find("EnemyPointer").GetComponent<RectTransform>();
+
+        // TODO: change this to use the calculate center of screen function
+        this.centerOfScreen = new Vector3(Screen.width / 2f, Screen.height / 2f, 0f) ;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // TODO: create a function the update the center of screen only when the screen size changes
+        // this.CalculateCenterOfScreen();
+
         this.MovePointer();
 
         this.RotatePointer();
+
+        this.isVisible();
     }
 
     private void MovePointer()
@@ -37,23 +45,21 @@ public class EnemyPointer : MonoBehaviour
             if (screenPosition.z < 0f)
                 screenPosition *= -1;
 
-            Vector3 centerOfScreen = new Vector3(Screen.width / 2f, Screen.height / 2f, 0f) ;
-
-            screenPosition -= centerOfScreen;
+            screenPosition -= this.centerOfScreen;
 
             float border = 50f;
 
-            if (screenPosition.x <= -centerOfScreen.x)
-                screenPosition.x = -centerOfScreen.x + border;
+            if (screenPosition.x <= -this.centerOfScreen.x)
+                screenPosition.x = -this.centerOfScreen.x + border;
 
-            if (screenPosition.x >= centerOfScreen.x)
-                screenPosition.x = centerOfScreen.x - border;
+            if (screenPosition.x >= this.centerOfScreen.x)
+                screenPosition.x = this.centerOfScreen.x - border;
 
-            if (screenPosition.y <= -centerOfScreen.y)
-                screenPosition.y = -centerOfScreen.y + border;
+            if (screenPosition.y <= -this.centerOfScreen.y)
+                screenPosition.y = -this.centerOfScreen.y + border;
 
-            if (screenPosition.y >= centerOfScreen.y)
-                screenPosition.y = centerOfScreen.y - border;
+            if (screenPosition.y >= this.centerOfScreen.y)
+                screenPosition.y = this.centerOfScreen.y - border;
 
             this.pointerRectTransform.localPosition = screenPosition;
         }
@@ -62,15 +68,27 @@ public class EnemyPointer : MonoBehaviour
     private void RotatePointer()
     {
         Vector3 targetPosition = Camera.main.WorldToScreenPoint(this.target.position);
-        Vector3 centerOfScreen = new Vector2(Screen.width / 2f, Screen.height / 2f);
 
         if (targetPosition.z < 0f)
                 targetPosition *= -1;
 
-        Vector2 dir = (targetPosition - centerOfScreen);
+        Vector2 dir = (targetPosition - this.centerOfScreen);
 
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
 
         this.pointerRectTransform.localEulerAngles = new Vector3(0f, 0f, angle);
+    }
+
+    private void isVisible() 
+    {
+        // int childIndex =  this.pointerRectTransform.transform.GetSiblingIndex();
+        // GameObject enemyPointer =  this.transform.GetChild(childIndex).gameObject;
+
+        GameObject enemyPointer = this.pointerRectTransform.gameObject;
+
+        if(this.isOffScreen)
+            enemyPointer.SetActive(true);
+        else
+            enemyPointer.SetActive(false);
     }
 }
