afb555e946c8a7375469f02739d56cc33429419c
Guy Erreich <guyerreich@gmail.com>
2022-04-02 21:37:58 +0300

    Added scriptable object for string prefabs

---
+++ Assets/LittleFlighter/Scripts/DataObjects/GameObjectsStorage.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DataObjects
+{
+    [CreateAssetMenu(fileName = "New Dictionary Storage", menuName = "Data Objects/Dictionary/Game Objects")]
+    public class GameObjectsStorage : ScriptableObject
+    {
+        [SerializeField] private List<string> keys;
+        [SerializeField] private List<GameObject> values;
+
+        public List<string> Keys { get => this.keys; }
+        public List<GameObject> Values { get => this.values; }
+    }
+}
