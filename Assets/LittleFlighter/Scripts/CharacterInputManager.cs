02a772debb2d97163f7cf7d0f221a70e7d8e1da9
Guy Erreich <guyerreich@gmail.com>
2022-04-09 23:32:22 +0300

    Some input system changes

--- Assets/LittleFlighter/Scripts/CharacterInputManager.cs
+++ Assets/LittleFlighter/Scripts/CharacterInputManager.cs
@@ -2,84 +2,95 @@ using UnityEngine;
 
 namespace LittleFlighter
 {
-    public class CharacterInputManager : MonoBehaviour {
+    public class CharacterInputManager : MonoBehaviour
+    {
         [SerializeField] private Texture2D cursor;
-        [SerializeField] [Range(1, 3)] private float looksensitivityY = 1, looksensitivityX = 1;
+        [SerializeField][Range(1, 3)] private float lookSensitivityY = 1, lookSensitivityX = 1;
         [SerializeField] private float deadzone = 0.05f;
         [SerializeField] private Camera cam;
 
         private SpacecraftController spacecraftController;
         private ProjectileLauncher projectileLauncher;
         private SheildAbility sheildAbility;
-
         private PlayerControls controls;
         private PlayerControls.CharacterActions characterInput;
 
-        private bool input_W, input_LeftMouse, input_Space, input_Dash = false;
+        private bool input_W, input_LeftMouse, input_Space, input_Dash;
+        private int dashCount = 1;
         private Vector2 mouseLook, movement;
 
-        private void Awake() 
+        #region Getters and Setters
+        public float LookSensitivityX 
+        {
+            get { return this.lookSensitivityX; }
+        }
+        #endregion
+
+        private void Awake()
         {
             Cursor.SetCursor(this.cursor, new Vector2(this.cursor.width / 2, this.cursor.height / 2),
                                 CursorMode.Auto);
 
-            spacecraftController =  this.GetComponent<SpacecraftController>();
-            projectileLauncher =  this.GetComponent<ProjectileLauncher>();
-            sheildAbility =  this.GetComponent<SheildAbility>();
+            spacecraftController = this.GetComponent<SpacecraftController>();
+            projectileLauncher = this.GetComponent<ProjectileLauncher>();
+            sheildAbility = this.GetComponent<SheildAbility>();
 
             this.controls = new PlayerControls();
             this.characterInput = this.controls.Character;
-            
+
             this.characterInput.Movement.performed += ctx => this.input_W = ctx.ReadValueAsButton();
             this.characterInput.NewMovement.performed += ctx => this.movement = ctx.ReadValue<Vector2>();
-            this.characterInput.Dash.performed += ctx => this.dashOnce(ctx.ReadValueAsButton());
+            this.characterInput.Dash.performed += ctx => this.input_Dash = ctx.ReadValueAsButton();
             this.characterInput.Look.performed += ctx => this.calculateMouseLook(ctx.ReadValue<Vector2>());
             this.characterInput.Attack.performed += ctx => this.input_LeftMouse = ctx.ReadValueAsButton();
             this.characterInput.Shield.performed += ctx => this.input_Space = ctx.ReadValueAsButton();
         }
 
-        private void Update() 
+        private void Update()
         {
             this.spacecraftController.ReceiveInput(this.input_W, this.movement, this.mouseLook, this.input_Dash);
+            this.input_Dash = false;  // Makes sure that u must click the dash button again for every dash
             this.projectileLauncher.ReceiveInput(this.input_LeftMouse);
             this.sheildAbility.ReceiveInput(this.input_Space);
         }
 
+        /// <summary>
+        /// Calculates a normalized value that represents how
+        /// far is the mouse from the center of screen.
+        /// While using a deadzone that represents an area where the value is 0.
+        /// <param name="mousePos">
+        ///     the current mouse position
+        /// </param>
+        /// </summary>
         private void calculateMouseLook(Vector2 mousePos)
         {
 
             Vector2 centerOfScreen = new Vector2(this.cam.pixelWidth / 2f, this.cam.pixelHeight / 2f);
 
             Vector2 mouseFromCenter = mousePos / centerOfScreen; // Relative to the middle of the screen in percntage (normalization)
-            
+
             mouseFromCenter = new Vector2(mouseFromCenter.x - 1f, mouseFromCenter.y - 1f); // Changes the middle of the screen to be (0,0)
 
-            if (Mathf.Abs(mouseFromCenter.x) < deadzone )
+            if (Mathf.Abs(mouseFromCenter.x) < deadzone)
                 mouseFromCenter.x = 0;
 
-            if (Mathf.Abs(mouseFromCenter.y) < deadzone )
+            if (Mathf.Abs(mouseFromCenter.y) < deadzone)
                 mouseFromCenter.y = 0;
 
             this.mouseLook.x = mouseFromCenter.x;
             this.mouseLook.y = -mouseFromCenter.y;
 
-            this.mouseLook.x *= this.looksensitivityX;
-            this.mouseLook.y *= this.looksensitivityY;
+            this.mouseLook.x *= this.lookSensitivityX;
+            this.mouseLook.y *= this.lookSensitivityY;
         }
 
-        private void dashOnce(bool isDash)
+        private void OnEnable()
         {
-            if (this.input_Dash != isDash)
-                 this.input_Dash = isDash; 
-            else if (this.input_Dash == isDash)
-                 this.input_Dash = false;
-        }
-
-        private void OnEnable() {
             controls.Enable();
         }
 
-        private void OnDestroy() {
+        private void OnDestroy()
+        {
             controls.Disable();
         }
     }
