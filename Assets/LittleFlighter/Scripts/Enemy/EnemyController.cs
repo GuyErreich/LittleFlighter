8157b48f1165c6378d3b0753447c77963a18c9a2
Guy Erreich <guyerreich@gmail.com>
2022-04-14 20:58:57 +0300

    Fixed issues in the enemy controller and add health to it

--- Assets/LittleFlighter/Scripts/Enemy/EnemyController.cs
+++ Assets/LittleFlighter/Scripts/Enemy/EnemyController.cs
@@ -1,34 +1,30 @@
-using System.Threading.Tasks;
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
-using DataObjects;
+using LittleFlighter.DataObjects;
 using LittleFlighter.Custom.Attributes;
+using LittleFlighter.System;
 
 namespace LittleFlighter.Enemy
 {
     public class EnemyController : MonoBehaviour, ISerializationCallbackReceiver
     {
         public static List<string> shootTypes;
-        private bool isInRange = false;
-        private Transform target, projectile;
-        private Quaternion nextRotation;
-        private float cooldownDuration = 0f; 
-
 
 
         [Header("References")]
         [SerializeField] private GameObjectsStorage shootTypesRefs;
+        [SerializeField] private Transform refEnemyModel;
         [SerializeField] private Transform refGunPivot;
 
 
         [Header("Defense Settings")]
-        [SerializeField] int health = 100;
+        [SerializeField] private int health = 100;
 
         
         [Header("Attack Settings")]
         [SerializeField, ListToPopup(typeof(EnemyController), "shootTypes")] private string type;
-        [SerializeField] private float damage = 5f;
+        [SerializeField] private int damage = 5;
         [SerializeField] private float projectileSpeed = 2f;
         [SerializeField] private float rateOfFire = 2f;
 
@@ -37,16 +33,29 @@ namespace LittleFlighter.Enemy
         [SerializeField] private float rotationSpeed = 3f;
 
 
+        private bool isInRange = false;
+        private Transform target;
+        private Quaternion nextRotation;
+        private float cooldownDuration = 0f; 
+        private Health healthComponent;
+
+        public event Action<float> OnHealthChanged;
+
 
         #region Getters and Setters
         public bool IsInRange { get {return this.isInRange; } }
-        #endregion
 
+        public int CurrentHealth { get; private set; }
+
+        #endregion
+        
 
 
         private void Awake()
         {
-            this.nextRotation = Random.rotation;
+            this.nextRotation = UnityEngine.Random.rotation;
+
+            this.CurrentHealth = this.health;
         }
 
         public void TrackMode()
@@ -66,7 +75,8 @@ namespace LittleFlighter.Enemy
             if (this.cooldownDuration <= 0f)
             {
                 int index = this.shootTypesRefs.Keys.IndexOf(this.type);
-                projectile = Instantiate(this.shootTypesRefs.Values[index]).transform;
+
+                var projectile = Instantiate(this.shootTypesRefs.Values[index]).transform;
 
                 projectile.gameObject.tag = "EnemyBullet";
 
@@ -74,7 +84,7 @@ namespace LittleFlighter.Enemy
                 projectile.transform.rotation = this.refGunPivot.transform.rotation;
 
                 Rigidbody projectileRbody = projectile.GetComponent<Rigidbody>();
-                projectileRbody.velocity = transform.forward * this.projectileSpeed;
+                projectileRbody.velocity = this.refEnemyModel.forward * this.projectileSpeed;
 
                 this.cooldownDuration = rateOfFire;
             }
@@ -84,22 +94,22 @@ namespace LittleFlighter.Enemy
 
         private void FollowTarget()
         {
-            Vector3 dir = this.target.position - this.transform.position;
+            Vector3 dir = this.target.position - this.refEnemyModel.position;
 
             this.nextRotation = Quaternion.LookRotation(dir);
 
-            this.transform.rotation =  Quaternion.Slerp(this.transform.rotation, this.nextRotation, this.rotationSpeed * Time.deltaTime);
+            this.refEnemyModel.rotation =  Quaternion.Slerp(this.refEnemyModel.rotation, this.nextRotation, this.rotationSpeed * Time.deltaTime);
         }
 
         private void RandomRoataion()
         {            
-            while (Quaternion.Angle(this.transform.rotation, this.nextRotation) == 0)
+            while (Quaternion.Angle(this.refEnemyModel.rotation, this.nextRotation) == 0)
             {
-                this.nextRotation = Random.rotation;
+                this.nextRotation = UnityEngine.Random.rotation;
             }
 
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, this.nextRotation, this.rotationSpeed * Time.deltaTime);
-        }
+            this.refEnemyModel.rotation = Quaternion.Slerp(this.refEnemyModel.rotation, this.nextRotation, this.rotationSpeed * Time.deltaTime);
+        } 
 
         private void OnTriggerEnter(Collider collider) 
         {
@@ -116,11 +126,37 @@ namespace LittleFlighter.Enemy
                 this.isInRange = false;
         }
 
+
+        #region Events Handlers
+
+        public void HandleHit(int amount, Collider collider)
+        {
+            if (collider.CompareTag("PlayerBullet"))
+                this.ModifyHealth( -amount );
+        }
+
+        #endregion Events Handlers
+
+        private void ModifyHealth(int amount)
+        {
+            this.CurrentHealth += amount;
+
+            float currentHealthPct = (float)this.CurrentHealth / (float)this.health;
+
+            this.OnHealthChanged?.Invoke(currentHealthPct);
+        }
+
+
+        #region Editor Control
+
         public void OnBeforeSerialize()
         {
             shootTypes = this.shootTypesRefs.Keys;
         }
 
+
         public void OnAfterDeserialize() {}
+
+        #endregion
     }
 }
